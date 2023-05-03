017535eb2bbaebe22669d7f5de24298aa5cbbb32
WallowitS190 <wallo190@gmail.com>
2023-05-03 16:07:05 -0500

    agrega clase usuario

--- app/src/main/java/com/example/veterinarycompany/MainActivity.java
+++ app/src/main/java/com/example/veterinarycompany/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.veterinarycompany;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombre,apellido,direccion,telefono,nacimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre = findViewById(R.id.ingresaNombre);
+        apellido = findViewById(R.id.ingresaapellido);
+        direccion = findViewById(R.id.ingresadireccion);
+        telefono = findViewById(R.id.ingresatelefono);
+        nacimiento = findViewById(R.id.ingresafecha);
+
+
     }
 }
---
+++ app/src/main/java/com/example/veterinarycompany/Usuario.java
@@ -0,0 +1,55 @@
+package com.example.veterinarycompany;
+
+public class Usuario {
+    private String nombre,apellido,direccion,fecha;
+    private int telefono;
+
+    public Usuario(String nombre, String apellido, String direccion, String fecha, int telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.fecha = fecha;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+}
