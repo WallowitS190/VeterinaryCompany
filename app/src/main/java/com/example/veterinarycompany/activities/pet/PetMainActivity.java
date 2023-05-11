ce8847a9017d6de4be1f6e7aee064fd8b91236e9
WallowitS190 <wallo190@gmail.com>
2023-05-11 17:38:34 -0500

    ajuste crear mascota

--- app/src/main/java/com/example/veterinarycompany/activities/pet/PetMainActivity.java
+++ app/src/main/java/com/example/veterinarycompany/activities/pet/PetMainActivity.java
@@ -2,6 +2,7 @@ package com.example.veterinarycompany.activities.pet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -29,8 +30,13 @@ public class PetMainActivity extends AppCompatActivity {
         binding.cardPetCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                onPetCreate();
             }
         });
+    }
+
+    private void onPetCreate(){
+        Intent intent = new Intent(this, CreatePetActivity.class);
+        startActivity(intent);
     }
 }
